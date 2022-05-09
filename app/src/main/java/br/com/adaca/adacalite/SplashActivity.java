400c5461d4d45622ac13d3ac3e4d8f3bb1ccb797
Bruno da silva <brunosilvamachado@id.uff.br>
2022-05-09 16:18:28 -0300

    Método de criar uma splashscreen foi atualizado

--- app/src/main/java/br/com/adaca/adacalite/SplashActivity.java
+++ app/src/main/java/br/com/adaca/adacalite/SplashActivity.java
@@ -1,28 +1,22 @@
 package br.com.adaca.adacalite;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.Bundle;
 import android.os.Handler;
-import android.view.View;
+import android.os.Looper;
 import android.widget.Toast;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
-
-import java.util.Objects;
 
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
-
-        hide();
-        Handler handler = new Handler();
+        Handler handler = new Handler(Looper.getMainLooper());
 
         Runnable entry = () -> {
             Intent intent = new Intent(SplashActivity.this, MainActivity.class);
@@ -37,32 +31,16 @@ public class SplashActivity extends AppCompatActivity {
                 .show();
 
         if (isOnline()){
-            handler.postDelayed(entry, 1000);
+            handler.postDelayed(entry, 1500);
         }
         else{
-            handler.postDelayed(exit, 2000);
+            handler.postDelayed(exit, 2750);
         }
     }
-
-    @Override
-    public void onWindowFocusChanged(boolean hasFocus) {
-        super.onWindowFocusChanged(hasFocus);
-        hide();
-    }
-
-    @SuppressLint("InlinedApi")
-    private void hide(){
-        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
-                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
-    }
-
     private boolean isOnline() {
         try {
             ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-            NetworkInfo networkInfo = Objects.requireNonNull(cm).getActiveNetworkInfo();
+            NetworkInfo networkInfo = cm.getActiveNetworkInfo();
             return networkInfo != null && networkInfo.isConnected();
         }catch (Exception e) {
             Toast.makeText(SplashActivity.this,R.string.no_connectivity_service,Toast.LENGTH_LONG).show();
